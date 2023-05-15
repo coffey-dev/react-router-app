dbacb2bbacc688d04dbd133e5e0ca26f3f180ef5
coffey-dev <scoffey@abc.gob.ar>
2023-05-15 18:25:17 -0300

    contextual errors

--- src/routes/destroy.jsx
+++ src/routes/destroy.jsx
@@ -2,6 +2,8 @@ import { redirect } from "react-router-dom";
 import { deleteContact } from "../contacts";
 
 export async function action({ params }) {
-  await deleteContact(params.contactId);
+    await deleteContact(params.contactId);
   return redirect("/");
+  throw new Error("oh dang!");
+ 
 }
