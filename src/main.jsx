28991f240b08eeacbb3b0887cae83a7d19bf85e6
coffey-dev <scoffey@abc.gob.ar>
2023-05-15 15:49:29 -0300

    Nested routes added

--- src/main.jsx
+++ src/main.jsx
@@ -6,12 +6,23 @@ import {
 } from "react-router-dom";
 import "./index.css";
 import Root from "../src/routes/root";
+import ErrorPage from "./error-page";
+import Contact from "./routes/contact";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+      },
+    ],
+
   },
+  
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
